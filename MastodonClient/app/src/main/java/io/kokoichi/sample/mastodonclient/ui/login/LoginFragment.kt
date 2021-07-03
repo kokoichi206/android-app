09724c14b503a42d090b5457922b03b9a7efb5ab
kokoichi <tt.300607@gmail.com>
2021-07-04 01:40:00 +0900

    feat: step41

--- MastodonClient/app/src/main/java/io/kokoichi/sample/mastodonclient/ui/login/LoginFragment.kt
+++ MastodonClient/app/src/main/java/io/kokoichi/sample/mastodonclient/ui/login/LoginFragment.kt
@@ -1,7 +1,10 @@
 package io.kokoichi.sample.mastodonclient.ui.login
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
@@ -25,10 +28,41 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
         )
     }
 
+    private val onObtainCode = fun(code: String) {
+
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val bindingData: FragmentLoginBinding? = DataBindingUtil.bind(view)
         binding = bindingData ?: return
+
+        val authUri = Uri.parse(BuildConfig.INSTANCE_URL)
+            .buildUpon()
+            .appendPath("oauth")
+            .appendPath("authorize")
+            .appendQueryParameter("client_id", BuildConfig.CLIENT_KEY)
+            .appendQueryParameter("redirect_uri", BuildConfig.CLIENT_REDIRECT_URI)
+            .appendQueryParameter("response_type", "code")
+            .appendQueryParameter("scope", BuildConfig.CLIENT_SCOPES)
+            .build()
+
+        bindingData.webview.webViewClient = InnerWebViewClient(onObtainCode)
+        bindingData.webview.settings.javaScriptEnabled = true
+        bindingData.webview.loadUrl(authUri.toString())
+    }
+    private class InnerWebViewClient (
+        val onObtainCode: (code: String) -> Unit
+    ) : WebViewClient() {
+        override fun onPageFinished(view: WebView?, url: String?) {
+            super.onPageFinished(view, url)
+            view ?: return
+
+            val code = Uri.parse(view.url).getQueryParameter("code")
+            code ?: return
+
+            onObtainCode(code)
+        }
     }
 }
