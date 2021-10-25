f63aa8b0147095299ed400dd8e7cef9956595551
kokoichi206 <tt.300607@gmail.com>
2021-10-26 00:50:32 +0900

    finish

--- MyApplication/app/src/main/java/com/example/myapplication/domain/use_case/get_coins/GetCoinsUseCase.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/domain/use_case/get_coins/GetCoinsUseCase.kt
@@ -3,6 +3,7 @@ package com.example.myapplication.domain.use_case.get_coins
 import com.example.myapplication.common.Resource
 import com.example.myapplication.data.remote.dto.toCoin
 import com.example.myapplication.domain.model.Coin
+import com.example.myapplication.domain.model.CoinDetail
 import com.example.myapplication.domain.repository.CoinRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -15,13 +16,13 @@ class GetCoinsUseCase @Inject constructor(
 ) {
     operator fun invoke(): Flow<Resource<List<Coin>>> = flow {
         try {
-            emit(Resource.Loading())
+            emit(Resource.Loading<List<Coin>>())
             val coins = repository.getCoins().map { it.toCoin() }
-            emit(Resource.Success(coins))
+            emit(Resource.Success<List<Coin>>(coins))
         } catch(e: HttpException) {
-            emit(Resource.Error(e.localizedMessage ?: "An unexpected error occured"))
+            emit(Resource.Error<List<Coin>>(e.localizedMessage ?: "An unexpected error occured"))
         } catch(e: IOException) {
-            emit(Resource.Error("Couldn't reach server. Check your internet conenction."))
+            emit(Resource.Error<List<Coin>>("Couldn't reach server. Check your internet conenction."))
         }
     }
 }
