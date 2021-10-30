9f324d6d96133053c81da2b56b77c7911147642d
kokoichi206 <tt.300607@gmail.com>
2021-10-31 05:53:28 +0900

    splash screen ui test

--- SplashScreen/app/src/main/java/com/example/splashscreen/presentation/splash/SplashScreen.kt
+++ SplashScreen/app/src/main/java/com/example/splashscreen/presentation/splash/SplashScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
@@ -17,11 +18,12 @@ import androidx.navigation.NavController
 import com.example.splashscreen.R
 import com.example.splashscreen.presentation.util.Screen
 import com.example.splashscreen.util.Constants.SPLASH_SCREEN_DURATION
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 
 @Composable
 fun SplashScreen(
-    navController: NavController
+    navController: NavController,
+    dispatcher: CoroutineDispatcher = Dispatchers.Main
 ) {
     val scale = remember {
         Animatable(0f)
@@ -30,18 +32,20 @@ fun SplashScreen(
         OvershootInterpolator(2f)
     }
     LaunchedEffect(key1 = true) {
-        scale.animateTo(
-            targetValue = 0.5f,
-            animationSpec = tween(
-                durationMillis = 500,
-                easing = {
-                    overshootInterpolator.getInterpolation(it)
-                }
+        withContext(dispatcher) {
+            scale.animateTo(
+                targetValue = 0.5f,
+                animationSpec = tween(
+                    durationMillis = 500,
+                    easing = {
+                        overshootInterpolator.getInterpolation(it)
+                    }
+                )
             )
-        )
-        delay(SPLASH_SCREEN_DURATION)
-        navController.popBackStack()
-        navController.navigate(Screen.LoginScreen.route)
+            delay(SPLASH_SCREEN_DURATION)
+            navController.popBackStack()
+            navController.navigate(Screen.LoginScreen.route)
+        }
     }
     Box(
         modifier = Modifier.fillMaxSize(),
