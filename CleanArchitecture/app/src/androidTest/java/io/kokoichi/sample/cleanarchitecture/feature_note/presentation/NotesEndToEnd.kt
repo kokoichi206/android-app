fda3e9c59663343c9385d7c1598ea806c9170050
kokoichi206 <tt.300607@gmail.com>
2021-10-31 14:33:26 +0900

    e2e testing

---
+++ CleanArchitecture/app/src/androidTest/java/io/kokoichi/sample/cleanarchitecture/feature_note/presentation/NotesEndToEnd.kt
@@ -0,0 +1,155 @@
+package io.kokoichi.sample.cleanarchitecture.feature_note.presentation
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import io.kokoichi.sample.cleanarchitecture.MainActivity
+import io.kokoichi.sample.cleanarchitecture.core.util.TestTags
+import io.kokoichi.sample.cleanarchitecture.feature_note.di.AppModule
+import io.kokoichi.sample.cleanarchitecture.feature_note.presentation.add_edit_note.AddEditNoteScreen
+import io.kokoichi.sample.cleanarchitecture.feature_note.presentation.notes.NotesScreen
+import io.kokoichi.sample.cleanarchitecture.feature_note.presentation.util.Screen
+import io.kokoichi.sample.cleanarchitecture.ui.theme.CleanArchitectureTheme
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+@UninstallModules(AppModule::class)
+class NotesEndToEnd {
+
+    @get:Rule(order = 0)
+    val hiltRule = HiltAndroidRule(this)
+
+    @get:Rule(order = 1)
+    val composeRule = createAndroidComposeRule<MainActivity>()
+
+    @ExperimentalAnimationApi
+    @Before
+    fun setUp() {
+        hiltRule.inject()
+        composeRule.setContent {
+            CleanArchitectureTheme {
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.NotesScreen.route
+                ) {
+                    composable(route = Screen.NotesScreen.route) {
+                        NotesScreen(navController = navController)
+                    }
+                    composable(
+                        route = Screen.AddEditNotesScreen.route +
+                                "?noteId={noteId}&noteColor={noteColor}",
+                        arguments = listOf(
+                            navArgument(
+                                name = "noteId"
+                            ) {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            },
+                            navArgument(
+                                name = "noteColor"
+                            ) {
+                                type = NavType.IntType
+                                defaultValue = -1
+                            },
+                        )
+                    ) {
+                        val color = it.arguments?.getInt("noteColor") ?: -1
+                        AddEditNoteScreen(
+                            navController = navController,
+                            noteColor = color
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    @Test
+    fun saveNewNote_editAfterwards() {
+        // Click on FAB to get to add note screen
+        composeRule.onNodeWithContentDescription("Add").performClick()
+
+        // Enter texts in title and content text fields
+        composeRule
+            .onNodeWithTag(TestTags.TITLE_TEXT_FIELD)
+            .performTextInput("test-title")
+        composeRule
+            .onNodeWithTag(TestTags.CONTENT_TEXT_FIELD)
+            .performTextInput("test-content")
+        composeRule.onNodeWithContentDescription("Save").performClick()
+
+        // Make sure there is a note in the list with our title and content
+        composeRule.onNodeWithText("test-title").assertIsDisplayed()
+        // Click on note to edit it
+        composeRule.onNodeWithText("test-title").performClick()
+
+        // Make sure  title and content texit fields contain note title and content
+        composeRule
+            .onNodeWithTag(TestTags.TITLE_TEXT_FIELD)
+            .assertTextEquals("test-title")
+        composeRule
+            .onNodeWithTag(TestTags.CONTENT_TEXT_FIELD)
+            .assertTextEquals("test-content")
+        // Add the text "2" to the title text field
+        composeRule
+            .onNodeWithTag(TestTags.TITLE_TEXT_FIELD)
+            .performTextInput("2")
+        // Update the note
+        composeRule.onNodeWithContentDescription("Save").performClick()
+
+        // Make sure the update was applied to the list
+        composeRule.onNodeWithText("test-title2").assertIsDisplayed()
+    }
+
+    @Test
+    fun saveNewNotes_orderByTitleDescending() {
+        for (i in 1..3) {
+            // Click on FAB to get to add note screen
+            composeRule.onNodeWithContentDescription("Add").performClick()
+
+            // Enter texts in title and content text fields
+            composeRule
+                .onNodeWithTag(TestTags.TITLE_TEXT_FIELD)
+                .performTextInput(i.toString())
+            composeRule
+                .onNodeWithTag(TestTags.CONTENT_TEXT_FIELD)
+                .performTextInput(i.toString())
+            composeRule.onNodeWithContentDescription("Save").performClick()
+        }
+
+        composeRule.onNodeWithText("1").assertIsDisplayed()
+        composeRule.onNodeWithText("2").assertIsDisplayed()
+        composeRule.onNodeWithText("3").assertIsDisplayed()
+
+        composeRule
+            .onNodeWithContentDescription("Sort")
+            .performClick()
+        composeRule
+            .onNodeWithContentDescription("Title")
+            .performClick()
+        composeRule
+            .onNodeWithContentDescription("Descending")
+            .performClick()
+
+        composeRule
+            .onAllNodesWithTag(TestTags.NOTE_ITEM)[0]
+            .assertTextContains("3")
+        composeRule
+            .onAllNodesWithTag(TestTags.NOTE_ITEM)[1]
+            .assertTextContains("2")
+        composeRule
+            .onAllNodesWithTag(TestTags.NOTE_ITEM)[2]
+            .assertTextContains("1")
+    }
+}
--- CleanArchitecture/app/src/main/java/io/kokoichi/sample/cleanarchitecture/feature_note/presentation/add_edit_note/components/TransparentHintTextField.kt
+++ CleanArchitecture/app/src/main/java/io/kokoichi/sample/cleanarchitecture/feature_note/presentation/add_edit_note/components/TransparentHintTextField.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusState
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.TextStyle
 
 @Composable
@@ -21,6 +22,7 @@ fun TransparentHintTextField(
     onValueChange: (String) -> Unit,
     textStyle: TextStyle = TextStyle(),
     singleLine: Boolean = false,
+    testTag: String = "",
     onFocusChange: (FocusState) -> Unit
 ) {
     Box(
@@ -29,8 +31,10 @@ fun TransparentHintTextField(
         BasicTextField(
             value = text,
             onValueChange = onValueChange,
+            singleLine = singleLine,
             textStyle = textStyle,
             modifier = Modifier
+                .testTag(testTag)
                 .fillMaxWidth()
                 .onFocusChanged {
                     onFocusChange(it)
