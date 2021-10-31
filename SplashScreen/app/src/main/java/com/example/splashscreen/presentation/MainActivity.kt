3812119e1b34127a55279a9918d51cb128faec1f
kokoichi206 <tt.300607@gmail.com>
2021-10-31 10:38:56 +0900

    implement login screen and login viewmodel

--- SplashScreen/app/src/main/java/com/example/splashscreen/presentation/MainActivity.kt
+++ SplashScreen/app/src/main/java/com/example/splashscreen/presentation/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
 import com.example.splashscreen.presentation.ui.theme.SplashScreenTheme
 import com.example.splashscreen.presentation.util.Navigation
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
