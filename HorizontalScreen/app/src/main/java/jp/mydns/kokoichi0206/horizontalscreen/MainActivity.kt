666f6fdd89063c69664f2f5d901c75f4e24e23c1
takahiro.tominaga <tt.300607@gmail.com>
2022-02-05 00:02:19 +0900

    horizontal screen project

---
+++ HorizontalScreen/app/src/main/java/jp/mydns/kokoichi0206/horizontalscreen/MainActivity.kt
@@ -0,0 +1,38 @@
+package jp.mydns.kokoichi0206.horizontalscreen
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import jp.mydns.kokoichi0206.horizontalscreen.ui.theme.HorizontalScreenTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            HorizontalScreenTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    HorizontalScreenTheme {
+        Greeting("Android")
+    }
+}
