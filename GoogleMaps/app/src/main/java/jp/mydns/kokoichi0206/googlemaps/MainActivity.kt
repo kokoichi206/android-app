edec91359fa4e5ff4ebdd2d3e64335dd52e969be
takahiro.tominaga <tt.300607@gmail.com>
2022-02-28 21:51:49 +0900

    di by hilt

--- GoogleMaps/app/src/main/java/jp/mydns/kokoichi0206/googlemaps/MainActivity.kt
+++ GoogleMaps/app/src/main/java/jp/mydns/kokoichi0206/googlemaps/MainActivity.kt
@@ -8,10 +8,16 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import dagger.hilt.android.lifecycle.HiltViewModel
+import jp.mydns.kokoichi0206.googlemaps.domain.repository.ParkingSpotRepository
 import jp.mydns.kokoichi0206.googlemaps.presentation.MapScreen
 import jp.mydns.kokoichi0206.googlemaps.ui.theme.GoogleMapsTheme
+import javax.inject.Inject
 
-class MainActivity : ComponentActivity() {
+@HiltViewModel
+class MainActivity @Inject constructor(
+    private val repository: ParkingSpotRepository
+) : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
---
+++ GoogleMaps/app/src/main/java/jp/mydns/kokoichi0206/googlemaps/ParkingSpotApplication.kt
@@ -0,0 +1,8 @@
+package jp.mydns.kokoichi0206.googlemaps
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ParkingSpotApplication : Application() {
+}
