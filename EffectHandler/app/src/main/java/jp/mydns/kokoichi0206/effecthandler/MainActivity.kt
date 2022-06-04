c4f601a3637864b3f5a032c46558be991738d812
takahiro.tominaga <tt.300607@gmail.com>
2022-06-04 16:05:34 +0900

    effect handler

--- EffectHandler/app/src/main/java/jp/mydns/kokoichi0206/effecthandler/MainActivity.kt
+++ EffectHandler/app/src/main/java/jp/mydns/kokoichi0206/effecthandler/MainActivity.kt
@@ -3,26 +3,94 @@ package jp.mydns.kokoichi0206.effecthandler
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.runtime.*
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import jp.mydns.kokoichi0206.effecthandler.ui.theme.EffectHandlerTheme
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             EffectHandlerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                var text by remember {
+                    mutableStateOf("")
+                }
+                EffectHandlerTheme {
+                    // LaunchedEffect is also Composable
+                    LaunchedEffect(key1 = text) {
+                        delay(1000L)
+                        println("The text is $text")
+                    }
                 }
             }
         }
+    }
+}
+
+@Composable
+fun RememberCoroutineScopeDemo() {
+    val scope = rememberCoroutineScope()
+    Button(onClick = {
+        scope.launch {
+            delay(1000L)
+            println("Hello world!")
+        }
+    }) {
+
+    }
+}
+
+@Composable
+fun RememberUpdatedStateDemo(
+    onTimeout: () -> Unit,
+) {
+    val updatedOnTimeout by rememberUpdatedState(newValue = onTimeout)
+    LaunchedEffect(true) {
+        delay(3000L)
+        onTimeout()
+    }
+}
+
+@Composable
+fun DisposableEffectDemo() {
+    val lifecycleOwner = LocalLifecycleOwner.current
+    DisposableEffect(key1 = lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            if (event == Lifecycle.Event.ON_PAUSE) {
+                println("On pause called")
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+}
+
+@Composable
+fun SideEffectDemo() {
+    SideEffect {
+        println("Called after every successful recomposition")
+    }
+}
+
+@Composable
+fun DerivedStateOfD() {
+    var counter by remember {
+        mutableStateOf(0)
+    }
+    val counterText by derivedStateOf {
+        "The counter is $counter"
+    }
+    Button(onClick = { counter++ }) {
+        Text(text = counterText)
     }
 }
 
