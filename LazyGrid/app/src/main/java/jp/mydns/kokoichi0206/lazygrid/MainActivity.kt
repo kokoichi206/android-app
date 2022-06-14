66295f0f9840a5a466dcd9dbc5999e58e141b7ce
takahiro.tominaga <tt.300607@gmail.com>
2022-06-14 20:56:08 +0900

    initial commit

---
+++ LazyGrid/app/src/main/java/jp/mydns/kokoichi0206/lazygrid/MainActivity.kt
@@ -0,0 +1,23 @@
+package jp.mydns.kokoichi0206.lazygrid
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import jp.mydns.kokoichi0206.lazygrid.ui.theme.LazyGridTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LazyGridTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                }
+            }
+        }
+    }
+}
