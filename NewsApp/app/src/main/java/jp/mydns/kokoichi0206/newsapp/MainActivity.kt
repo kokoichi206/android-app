49455805e351a7f1cd4451b29bc6644060649b3e
takahiro.tominaga <tt.300607@gmail.com>
2022-07-01 23:56:10 +0900

    first commit

---
+++ NewsApp/app/src/main/java/jp/mydns/kokoichi0206/newsapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package jp.mydns.kokoichi0206.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import jp.mydns.kokoichi0206.newsapp.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NewsAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                }
+            }
+        }
+    }
+}
---
+++ memo/solid.md
@@ -0,0 +1,7 @@
+## solid
+[this youtube](https://www.youtube.com/watch?v=t8VTLxMsufU&list=RDCMUCKNTZMRHPLXfqlbdOI7mCkg&start_radio=1&rv=t8VTLxMsufU&t=98&ab_channel=PhilippLackner)
+
+
+Single responsibility.
+responsibility is reason to change
+
