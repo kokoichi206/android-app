6b83df7e68380e823ef85df7c136d4ed9d940260
takahiro.tominaga <tt.300607@gmail.com>
2022-07-06 07:14:04 +0900

    forecast screen

--- weatherapp/app/src/main/java/jp/mydns/kokoichi0206/weatherapp/MainActivity.kt
+++ weatherapp/app/src/main/java/jp/mydns/kokoichi0206/weatherapp/MainActivity.kt
@@ -8,11 +8,17 @@ import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import dagger.hilt.android.AndroidEntryPoint
 import jp.mydns.kokoichi0206.weatherapp.presentation.WeatherCard
+import jp.mydns.kokoichi0206.weatherapp.presentation.WeatherForecast
 import jp.mydns.kokoichi0206.weatherapp.presentation.WeatherViewModel
 import jp.mydns.kokoichi0206.weatherapp.presentation.ui.theme.DarkBlue
 import jp.mydns.kokoichi0206.weatherapp.presentation.ui.theme.DeepBlue
@@ -32,23 +38,46 @@ class MainActivity : ComponentActivity() {
         ) {
             viewModel.loadWeatherInfo()
         }
-        permissionLauncher.launch(arrayOf(
-            Manifest.permission.ACCESS_FINE_LOCATION,
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-        ))
+        permissionLauncher.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            )
+        )
 
         setContent {
             WeatherAppTheme {
-                Column(
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .background(DarkBlue),
+
+                Box(
+                    modifier = Modifier.fillMaxSize(),
                 ) {
-                    WeatherCard(
-                        state = viewModel.state,
-                        backgroundColor = DeepBlue,
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(DarkBlue),
+                    ) {
+                        WeatherCard(
+                            state = viewModel.state,
+                            backgroundColor = DeepBlue,
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        WeatherForecast(state = viewModel.state)
+                    }
+                    if (viewModel.state.isLoading) {
+                        CircularProgressIndicator(
+                            modifier = Modifier.align(Alignment.Center),
+                        )
+                    }
+                    viewModel.state.error?.let { error ->
+                        Text(
+                            text = error,
+                            color = Color.Red,
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier.align(Alignment.Center),
+                        )
+                    }
                 }
+
             }
         }
     }
