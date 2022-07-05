d54cff17822e471c5f9732c09669581354cd1be9
takahiro.tominaga <tt.300607@gmail.com>
2022-07-06 06:52:33 +0900

    today's weather

---
+++ weatherapp/README.md
@@ -0,0 +1,2 @@
+## Weather App
+Thank you for this [youtube](https://www.youtube.com/watch?v=eAbKK7JNxCE&ab_channel=PhilippLackner)
--- weatherapp/app/src/main/java/jp/mydns/kokoichi0206/weatherapp/MainActivity.kt
+++ weatherapp/app/src/main/java/jp/mydns/kokoichi0206/weatherapp/MainActivity.kt
@@ -1,21 +1,53 @@
 package jp.mydns.kokoichi0206.weatherapp
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
+import jp.mydns.kokoichi0206.weatherapp.presentation.WeatherCard
+import jp.mydns.kokoichi0206.weatherapp.presentation.WeatherViewModel
+import jp.mydns.kokoichi0206.weatherapp.presentation.ui.theme.DarkBlue
+import jp.mydns.kokoichi0206.weatherapp.presentation.ui.theme.DeepBlue
 import jp.mydns.kokoichi0206.weatherapp.presentation.ui.theme.WeatherAppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: WeatherViewModel by viewModels()
+    private lateinit var permissionLauncher: ActivityResultLauncher<Array<String>>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        permissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            viewModel.loadWeatherInfo()
+        }
+        permissionLauncher.launch(arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+        ))
+
         setContent {
             WeatherAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(DarkBlue),
+                ) {
+                    WeatherCard(
+                        state = viewModel.state,
+                        backgroundColor = DeepBlue,
+                    )
                 }
             }
         }
