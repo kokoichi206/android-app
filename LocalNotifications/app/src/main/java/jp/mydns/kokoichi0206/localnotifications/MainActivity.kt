2e12974f8d7d218a2f9b1c8dad475b326a79c0ab
takahiro.tominaga <tt.300607@gmail.com>
2022-09-19 00:19:07 +0900

    initial commit

---
+++ LocalNotifications/app/src/main/java/jp/mydns/kokoichi0206/localnotifications/MainActivity.kt
@@ -0,0 +1,23 @@
+package jp.mydns.kokoichi0206.localnotifications
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import jp.mydns.kokoichi0206.localnotifications.ui.theme.LocalNotificationsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LocalNotificationsTheme {
+            }
+        }
+    }
+}
