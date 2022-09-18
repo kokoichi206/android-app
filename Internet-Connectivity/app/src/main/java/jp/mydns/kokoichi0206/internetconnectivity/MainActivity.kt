f0ca227b8bc1dad7ac79910fb51af264e7d1d206
takahiro.tominaga <tt.300607@gmail.com>
2022-09-18 21:05:08 +0900

    internet connectivity

--- Internet-Connectivity/app/src/main/java/jp/mydns/kokoichi0206/internetconnectivity/MainActivity.kt
+++ Internet-Connectivity/app/src/main/java/jp/mydns/kokoichi0206/internetconnectivity/MainActivity.kt
@@ -3,22 +3,46 @@ package jp.mydns.kokoichi0206.internetconnectivity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import jp.mydns.kokoichi0206.internetconnectivity.ui.theme.InternetConnectivityTheme
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var connectivityObserver: ConnectivityObserver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        connectivityObserver = NetworkConnectivityObserver(applicationContext)
+        // Non composable app
+        connectivityObserver.observe().onEach {
+            println("Status is $it")
+        }.launchIn(lifecycleScope)
         setContent {
             InternetConnectivityTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    val status by connectivityObserver.observe().collectAsState(
+                        initial = ConnectivityObserver.Status.Unavailable
+                    )
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text(text = "Network status: $status")
+                    }
                 }
             }
         }
