6fc0c5ab60c1b6bc98a9b5b007ea610f086c63db
takahiro.tominaga <tt.300607@gmail.com>
2022-09-19 07:18:49 +0900

    Jetpack Composeの導入

--- KMMTest/androidApp/build.gradle.kts
+++ KMMTest/androidApp/build.gradle.kts
@@ -17,6 +17,13 @@ android {
             isMinifyEnabled = false
         }
     }
+    buildFeatures {
+        // Enables Jetpack Compose for this module
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.3.0"
+    }
 }
 
 dependencies {
@@ -24,4 +31,17 @@ dependencies {
     implementation("com.google.android.material:material:1.4.0")
     implementation("androidx.appcompat:appcompat:1.3.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.0")
+
+    // Integration with activities
+    implementation("androidx.activity:activity-compose:1.5.1")
+    // Compose Material Design
+    implementation("androidx.compose.material:material:1.2.1")
+    // Animations
+    implementation("androidx.compose.animation:animation:1.2.1")
+    // Tooling support (Previews, etc.)
+    implementation("androidx.compose.ui:ui-tooling:1.2.1")
+    // Integration with ViewModels
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.5.1")
+    // UI Tests
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.2.1")
 }
--- KMMTest/androidApp/src/main/java/jp/mydns/kokoichi0206/kmmtest/android/MainActivity.kt
+++ KMMTest/androidApp/src/main/java/jp/mydns/kokoichi0206/kmmtest/android/MainActivity.kt
@@ -2,8 +2,13 @@ package jp.mydns.kokoichi0206.kmmtest.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import jp.mydns.kokoichi0206.kmmtest.Greeting
-import android.widget.TextView
 
 fun greet(): String {
     return Greeting().greeting()
@@ -12,9 +17,17 @@ fun greet(): String {
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = greet()
+        setContent {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center,
+            ) {
+                Text(
+                    text = greet()
+                )
+            }
+        }
     }
 }
