2929368bebdf27604456f6b20c39f68bd1f44ef9
takahiro.tominaga <tt.300607@gmail.com>
2022-10-05 19:43:09 +0900

    initial commit

---
+++ Canvas/app/src/main/java/jp/mydns/kokoichi0206/canvas/MainActivity.kt
@@ -0,0 +1,16 @@
+package jp.mydns.kokoichi0206.canvas
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import jp.mydns.kokoichi0206.canvas.ui.theme.CanvasTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CanvasTheme {
+            }
+        }
+    }
+}
