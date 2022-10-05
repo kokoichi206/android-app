c779a078b4f1f023bce269005e18fdc8c5cb734f
takahiro.tominaga <tt.300607@gmail.com>
2022-10-06 00:18:54 +0900

    canvasをギャラリーに保存する

--- Canvas/README.md
+++ Canvas/README.md
@@ -2,3 +2,6 @@
 
 DroidKaigi での[こちらの発表](https://github.com/kkkkan/ComposeAppForDroidKaigi2022)をもとに勉強させてもらいました。
 ありがとうございます。
+
+保存方法については、[こちらの youtube](https://www.youtube.com/watch?v=Ul4hum3y0J8&ab_channel=AndroidTutorials) を参考にしました。
+ありがとうございます。
--- Canvas/app/src/main/java/jp/mydns/kokoichi0206/canvas/MainActivity.kt
+++ Canvas/app/src/main/java/jp/mydns/kokoichi0206/canvas/MainActivity.kt
@@ -1,17 +1,100 @@
 package jp.mydns.kokoichi0206.canvas
 
+import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import jp.mydns.kokoichi0206.canvas.ui.theme.CanvasTheme
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
+
+    private val REQUEST_CODE = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CanvasTheme {
-                EditPictureView()
+                val snapShot = CaptureBitmap {
+                    EditPictureView()
+                }
+                Box(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Button(
+                        onClick = {
+                            MainScope().launch {
+                                val bitmap = snapShot.invoke()
+                                onClick(bitmap)
+                            }
+                        }) {
+                        Text(text = "Capture")
+                    }
+                }
             }
+        }
+    }
+
+    private fun onClick(bitmap: Bitmap) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            saveImageToGallery(bitmap)
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE
+                ), REQUEST_CODE
+            )
+        }
+    }
+
+    private fun saveImageToGallery(bitmap: Bitmap) {
+
+        val contentResolver = contentResolver
+
+        val images = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY)
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+
+        val contentValues = ContentValues()
+        contentValues.put(MediaStore.Images.Media.DISPLAY_NAME, "${System.currentTimeMillis()}.jpg")
+        contentValues.put(MediaStore.Images.Media.MIME_TYPE, "images/")
+
+        val uri = contentResolver.insert(images, contentValues)
+
+        try {
+            uri?.let {
+                val outputStream = contentResolver.openOutputStream(uri)
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+
+                Toast.makeText(this, "Images Saved Successfully", Toast.LENGTH_SHORT).show()
+            }
+        } catch (e: Exception) {
+            Toast.makeText(this, "Images not Saved", Toast.LENGTH_SHORT).show()
+            e.printStackTrace()
         }
     }
 }
