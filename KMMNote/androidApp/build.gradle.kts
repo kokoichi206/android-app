12d9ca3e1bc8f17b154660e9ba33442673ac6b0d
takahiro.tominaga <tt.300607@gmail.com>
2022-10-06 17:34:00 +0900

    setup dependencies

--- KMMNote/androidApp/build.gradle.kts
+++ KMMNote/androidApp/build.gradle.kts
@@ -1,15 +1,17 @@
 plugins {
     id("com.android.application")
     kotlin("android")
+    id("kotlin-kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
     namespace = "jp.mydns.kokoichi0206.kmmnote.android"
-    compileSdk = 32
+    compileSdk = 33
     defaultConfig {
         applicationId = "jp.mydns.kokoichi0206.kmmnote.android"
         minSdk = 26
-        targetSdk = 32
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
     }
@@ -18,6 +20,9 @@ android {
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.3.0"
+    }
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
     }
     packagingOptions {
         resources {
@@ -38,5 +43,14 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
     implementation("androidx.compose.foundation:foundation:1.2.1")
     implementation("androidx.compose.material:material:1.2.1")
-    implementation("androidx.activity:activity-compose:1.5.1")
+    implementation("androidx.activity:activity-compose:1.6.0")
+    // Maybe get error for latest version ??
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
+
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
+
+    implementation("com.google.dagger:hilt-android:2.42")
+    kapt("com.google.dagger:hilt-android-compiler:2.42")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
 }
