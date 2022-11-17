0b61969e23ba7c810721fc40c9bdcb0a930c313c
takahiro.tominaga <tt.300607@gmail.com>
2022-11-18 06:27:50 +0900

    save image to internal storage

--- PlayGround/app/src/main/java/jp/mydns/kokoichi0206/playground/MyImagePicker.kt
+++ PlayGround/app/src/main/java/jp/mydns/kokoichi0206/playground/MyImagePicker.kt
@@ -1,6 +1,8 @@
 package jp.mydns.kokoichi0206.playground
 
+import android.content.Context
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.net.Uri
 import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -9,10 +11,7 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -28,14 +27,18 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import coil.compose.rememberImagePainter
+import java.io.File
 
 @Composable
 fun MyImagePicker() {
 
     val context = LocalContext.current
+    val fileName = "test_file.jpg"
+    val defaultUri = "android.resource://${context.packageName}/drawable/ic_person"
 
     var selectedImage by remember {
         mutableStateOf(Uri.EMPTY)
+//        mutableStateOf(Uri.parse(defaultUri))
     }
 
     val picker = rememberLauncherForActivityResult(
@@ -49,24 +52,54 @@ fun MyImagePicker() {
                 uri,
                 Intent.FLAG_GRANT_READ_URI_PERMISSION,
             )
+
+            val fos = context.openFileOutput(fileName, Context.MODE_PRIVATE)
+            context.contentResolver.openInputStream(uri)?.use { inputStream ->
+                fos.write(inputStream.readBytes())
+            }
         }
     }
+    LaunchedEffect(key1 = true) {
+        context.getFileStreamPath(fileName)?.let { file ->
+            Log.d("hoge", "file.isFile: ${file.isFile}")
+            Log.d("hoge", "file.freeSpace: ${file.freeSpace}")
+            if (file.isFile) {
+                selectedImage = Uri.fromFile(file)
+            }
+        }
+    }
+//    try {
+//        val file = context.getFileStreamPath(fileName)
+//        selectedImage = Uri.fromFile(file)
+//    } catch (e: Exception) {
+//        e.localizedMessage?.let {
+//            Log.d("hoge", it)
+//        }
+//    }
 
 //    val defaultUri = "content://media/picker/0/com.android.providers.media.photopicker/media/1000000029"
-    val defaultUri = "android.resource://${context.packageName}/drawable/ic_person"
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center,
     ) {
-        Column {
-            Button(
-                onClick = {
-                    picker.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-                }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Box(
+                modifier = Modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center,
             ) {
-                Text(
-                    text = "Click to load Image"
-                )
+                Button(
+                    onClick = {
+                        selectedImage = Uri.EMPTY
+                        context.deleteFile(fileName)
+                    },
+                ) {
+                    Text(
+                        text = "Reset Image"
+                    )
+                }
             }
             Image(
                 modifier = Modifier
@@ -82,6 +115,7 @@ fun MyImagePicker() {
                     } else {
                         selectedImage
                     }
+//                    selectedImage
                 ),
                 contentDescription = "",
                 contentScale = ContentScale.Crop,
