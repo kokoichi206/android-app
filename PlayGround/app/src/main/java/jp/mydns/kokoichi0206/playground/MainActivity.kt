f659a0b62e7701c7e11dbd9543c149b496d35b65
takahiro.tominaga <tt.300607@gmail.com>
2022-11-17 18:16:29 +0900

    storage

--- PlayGround/app/src/main/java/jp/mydns/kokoichi0206/playground/MainActivity.kt
+++ PlayGround/app/src/main/java/jp/mydns/kokoichi0206/playground/MainActivity.kt
@@ -1,6 +1,8 @@
 package jp.mydns.kokoichi0206.playground
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Environment
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -19,10 +21,18 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import jp.mydns.kokoichi0206.playground.blogs.MaterialYouTest
 import jp.mydns.kokoichi0206.playground.ui.theme.PlayGroundTheme
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.File
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        storage(this)
+
         setContent {
             PlayGroundTheme {
                 // A surface container using the 'background' color from the theme
@@ -69,6 +79,31 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+// https://www.youtube.com/watch?v=jcO6p5TlcGs&list=PLWz5rJ2EKKc8PO99T1QQLrPAJILqxJXW6&index=8&ab_channel=AndroidDevelopers
+fun storage(context: Context) {
+
+    runBlocking(Dispatchers.IO) {
+        val url = ""
+        val client = OkHttpClient()
+        val request = Request.Builder().url(url).build()
+
+        // use を使うと、勝手に閉じてくれるのでメモリリークの心配がない
+        client.newCall(request).execute().use { response ->
+            response.body?.byteStream()?.use { input ->
+                val target = File(context.filesDir, "members.json")
+
+                target.outputStream().use { output ->
+                    input.copyTo(output)
+                }
+            }
+        }
+    }
+
+//    val type = Environment.DIRECTORY_PICTURES
+//    val target = Environment.getExternalStorageDirectory(type)
+//    val newImage = File(target, "new-image-by-playground")
 }
 
 @Composable
