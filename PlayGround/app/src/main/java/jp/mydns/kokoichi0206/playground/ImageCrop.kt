f971f97ea8e96547ba41185dc3efab107775b13d
takahiro.tominaga <tt.300607@gmail.com>
2022-11-19 23:02:10 +0900

    save and reuse cropped image

--- PlayGround/app/src/main/java/jp/mydns/kokoichi0206/playground/ImageCrop.kt
+++ PlayGround/app/src/main/java/jp/mydns/kokoichi0206/playground/ImageCrop.kt
@@ -1,33 +1,49 @@
 package jp.mydns.kokoichi0206.playground
 
+import android.content.Context
 import android.content.res.Resources
+import android.graphics.Bitmap
+import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.border
+import androidx.compose.foundation.gestures.TransformableState
 import androidx.compose.foundation.gestures.detectTransformGestures
 import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.gestures.transformable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.drawToBitmap
+import coil.compose.rememberAsyncImagePainter
+import java.io.ByteArrayOutputStream
 
 @Composable
 fun ImageCrop() {
+    var saved by remember {
+        mutableStateOf(false)
+    }
+    var bitmap by remember {
+        mutableStateOf(Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888))
+    }
 
     var scale by remember {
         mutableStateOf(1f)
@@ -40,16 +56,24 @@ fun ImageCrop() {
     }
     val state = rememberTransformableState { zoomChange, _, _ ->
         scale *= zoomChange
-        if(scale < 1){
+        if (scale < 1) {
             scale = 1F
         }
     }
     Log.d("hoge", "scale: $scale")
-    Box(
-        modifier = Modifier
-            .fillMaxSize()
-            .pointerInput(Unit) {
-                detectTransformGestures { centroid, pan, zoom, rotation ->
+
+    if (saved) {
+        ImageCropped(bitmap = bitmap) {
+            saved = !saved
+        }
+    } else {
+        val snapShot = CaptureBitmap {
+            ImageCropDoing(
+                translateX = translateX,
+                translateY = translateY,
+                scale = scale,
+                state = state,
+                onDetectTransformGestures = { pan, zoom ->
                     scale = when {
                         scale < 0.5f -> 0.5f
                         scale > 3f -> 3f
@@ -57,8 +81,102 @@ fun ImageCrop() {
                     }
                     translateX += pan.x
                     translateY += pan.y
-                    Log.d("hoge", "translateX: $translateX")
-                    Log.d("hoge", "translateY: $translateY")
+                },
+                onSaveButtonClicked = {
+                    saved = !saved
+                }
+            )
+        }
+
+        val context = LocalContext.current
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.TopEnd
+        ) {
+            Button(onClick = {
+                saved = !saved
+                if (saved) {
+                    bitmap = snapShot.invoke()
+
+                    val bytes = ByteArrayOutputStream()
+                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, bytes)
+
+                    val fos = context.openFileOutput("cropped_image_test.jpg", Context.MODE_PRIVATE)
+                    fos.write(bytes.toByteArray())
+                }
+            }) {
+                Text(text = "Save")
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageCropped(
+    bitmap: Bitmap,
+    onSaveButtonClicked: () -> Unit,
+) {
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.TopEnd,
+        ) {
+            Button(onClick = {
+                onSaveButtonClicked()
+            }) {
+                Text(text = "Back")
+            }
+        }
+
+        Image(
+            bitmap = bitmap.asImageBitmap(),
+            contentDescription = "",
+            modifier = Modifier
+                .size(200.dp)
+                .clip(CircleShape)
+                .scale(5f / 4)  // 最初円を描画するときに、1/5 だけスペースを開けているため！
+                .border(0.dp, Color.Transparent, CircleShape),
+            contentScale = ContentScale.Crop,
+        )
+
+        LocalContext.current.getFileStreamPath("cropped_image_test.jpg")?.let { file ->
+            Log.d("hoge", "file.isFile: ${file.isFile}")
+            Log.d("hoge", "file.freeSpace: ${file.freeSpace}")
+
+            Image(
+                painter = rememberAsyncImagePainter(Uri.fromFile(file)),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape)
+                    .scale(5f / 4)  // 最初円を描画するときに、1/5 だけスペースを開けているため！
+                    .border(0.dp, Color.Transparent, CircleShape),
+                contentScale = ContentScale.Crop,
+            )
+        }
+    }
+}
+
+@Composable
+fun ImageCropDoing(
+    translateX: Float,
+    translateY: Float,
+    scale: Float,
+    state: TransformableState,
+    onDetectTransformGestures: (pan: Offset, zoom: Float) -> Unit,
+    onSaveButtonClicked: () -> Unit,
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .pointerInput(Unit) {
+                detectTransformGestures { centroid, pan, zoom, rotation ->
+                    onDetectTransformGestures(pan, zoom)
                     Log.d("hoge", "centroid: $centroid")
                     Log.d("hoge", "change: $pan")
                     Log.d("hoge", "rotation: $rotation")
@@ -72,15 +190,6 @@ fun ImageCrop() {
                     scaleX = scale,
                     scaleY = scale
                 )
-//                .pointerInput(Unit) {
-//                    detectDragGestures { change, dragAmount ->
-//                        translateX += dragAmount.x * scale
-//                        translateY += dragAmount.y * scale
-//                        Log.d("hoge", "translateX: $translateX")
-//                        Log.d("hoge", "translateY: $translateY")
-//                        Log.d("hoge", "change: $change")
-//                    }
-//                }
                 .offset(
                     x = translateX.toDp(),
                     y = translateY.toDp(),
@@ -119,4 +228,39 @@ fun ImageCrop() {
 
 fun Float.toDp(): Dp {
     return (this / Resources.getSystem().displayMetrics.density).dp
+}
+
+@Composable
+fun CaptureBitmap(
+    content: @Composable () -> Unit,
+): () -> Bitmap {
+
+    val context = LocalContext.current
+
+    /**
+     * ComposeView that would take composable as its content
+     * Kept in remember so recomposition doesn't re-initialize it
+     **/
+    val composeView = remember { ComposeView(context) }
+
+    /**
+     * Callback function which could get latest image bitmap
+     **/
+    fun captureBitmap(): Bitmap {
+        return composeView.drawToBitmap()
+    }
+
+    /** Use Native View inside Composable **/
+    AndroidView(
+        factory = {
+            composeView.apply {
+                setContent {
+                    content.invoke()
+                }
+            }
+        }
+    )
+
+    /** returning callback to bitmap **/
+    return ::captureBitmap
 }
