ca2de21c053a1a10ffaa8afd25344a0747507cd8
takahiro.tominaga <tt.300607@gmail.com>
2022-12-23 01:28:17 +0900

    PickVisualMedia

--- photo-picker/app/src/main/java/jp/mydns/kokoichi0206/photo_picker/MainActivity.kt
+++ photo-picker/app/src/main/java/jp/mydns/kokoichi0206/photo_picker/MainActivity.kt
@@ -1,15 +1,27 @@
 package jp.mydns.kokoichi0206.photo_picker
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import coil.compose.AsyncImage
 import jp.mydns.kokoichi0206.photo_picker.ui.theme.PhotopickerTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +29,72 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             PhotopickerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                var selectedImageUri by remember {
+                    mutableStateOf<Uri?>(null)
+                }
+                var selectedImageUris by remember {
+                    mutableStateOf<List<Uri>>(emptyList())
+                }
+
+                val singlePhotoPickerLauncher = rememberLauncherForActivityResult(
+                    // https://developer.android.com/reference/androidx/activity/result/contract/ActivityResultContracts.PickVisualMedia
+                    contract = ActivityResultContracts.PickVisualMedia(),
+                    onResult = { uri -> selectedImageUri = uri },
+                )
+                val multiplePhotosPickerLauncher = rememberLauncherForActivityResult(
+                    contract = ActivityResultContracts.PickMultipleVisualMedia(),
+                    onResult = { uris -> selectedImageUris = uris },
+                )
+
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxSize()
+                ) {
+                    item {
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceAround,
+                        ) {
+                            Button(
+                                onClick = {
+                                    singlePhotoPickerLauncher.launch(
+                                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                                    )
+                                },
+                            ) {
+                                Text(text = "Pick one photo")
+                            }
+
+                            Button(
+                                onClick = {
+                                    multiplePhotosPickerLauncher.launch(
+                                        PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly)
+                                    )
+                                },
+                            ) {
+                                Text(text = "Pick multiple photos")
+                            }
+                        }
+                    }
+
+                    item {
+                        AsyncImage(
+                            model = selectedImageUri,
+                            contentDescription = "",
+                            modifier = Modifier.fillMaxWidth(),
+                            contentScale = ContentScale.Crop,
+                        )
+                    }
+
+                    items(selectedImageUris) { uri ->
+                        AsyncImage(
+                            model = uri,
+                            contentDescription = "",
+                            modifier = Modifier.fillMaxWidth(),
+                            contentScale = ContentScale.Crop,
+                        )
+                    }
                 }
             }
         }
