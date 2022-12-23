57f8ee930fe23b497897f21f3babe7915010ba02
takahiro.tominaga <tt.300607@gmail.com>
2022-12-24 07:14:15 +0900

    Datetime api

--- photo-picker/app/src/main/java/jp/mydns/kokoichi0206/photo_picker/MainActivity.kt
+++ photo-picker/app/src/main/java/jp/mydns/kokoichi0206/photo_picker/MainActivity.kt
@@ -2,6 +2,7 @@ package jp.mydns.kokoichi0206.photo_picker
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
@@ -23,12 +24,38 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.AsyncImage
 import jp.mydns.kokoichi0206.photo_picker.ui.theme.PhotopickerTheme
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             PhotopickerTheme {
+                val date = remember {
+                    // 2022-12-24T05:21:03.512451+09:00[Asia/Tokyo]
+                    ZonedDateTime.now()
+//                        .toEpochSecond() * 1000
+//                    ZonedDateTime.ofInstant(
+//                        Instant.ofEpochMilli(System.currentTimeMillis()),
+//                        ZoneId.of("Asia/Tokyo"),
+//                    )
+
+                //                    LocalDateTime.now()
+//                        .plusDays(13)
+                }
+                Log.d("hoge", date.toString())
+
+                val formattedDateTime = remember {
+                    DateTimeFormatter
+                        .ofPattern("EEE dd MM yyy HH:mm:ss")
+                        .format(date)
+                }
+                Log.d("hoge", formattedDateTime.toString())
+
                 var selectedImageUri by remember {
                     mutableStateOf<Uri?>(null)
                 }
