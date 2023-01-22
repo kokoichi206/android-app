dac6047aef079da470b06e01a53f089b17277327
takahiro.tominaga <tt.300607@gmail.com>
2023-01-22 21:57:04 +0900

    Shimmer Effect

--- ShsimmerEffect/app/src/main/java/jp/mydns/kokoichi0206/shsimmereffect/MainActivity.kt
+++ ShsimmerEffect/app/src/main/java/jp/mydns/kokoichi0206/shsimmereffect/MainActivity.kt
@@ -3,14 +3,18 @@ package jp.mydns.kokoichi0206.shsimmereffect
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.List
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import jp.mydns.kokoichi0206.shsimmereffect.ui.theme.ShsimmerEffectTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +23,56 @@ class MainActivity : ComponentActivity() {
             ShsimmerEffectTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    var isLoading by remember {
+                        mutableStateOf(true)
+                    }
+                    LaunchedEffect(key1 = true) {
+                        delay(10000)
+                        isLoading = false
+                    }
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize(),
+                    ) {
+                        item {
+                            Box(
+                                modifier = Modifier.fillMaxWidth(),
+                                contentAlignment = Alignment.Center,
+                            ) {
+                                Button(
+                                    onClick = {
+                                  isLoading = true
+                                },
+                                ) {
+                                    Text(text = "Reload")
+                                }
+                            }
+                        }
+                        items(20) {
+                            ShimmerListItem(
+                                isLoading = isLoading,
+                                contentAfterLoading = {
+                                    Row(
+                                        modifier = Modifier
+                                            .fillMaxWidth()
+                                            .padding(16.dp)
+                                    ) {
+                                        Icon(
+                                            imageVector = Icons.Default.List,
+                                            contentDescription = null,
+                                            modifier = Modifier.size(100.dp),
+                                        )
+                                        Spacer(modifier = Modifier.width(16.dp))
+                                        Text(
+                                            text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unkn"
+                                        )
+                                    }
+                                },
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .padding(16.dp)
+                            )
+                        }
+                    }
                 }
             }
         }
