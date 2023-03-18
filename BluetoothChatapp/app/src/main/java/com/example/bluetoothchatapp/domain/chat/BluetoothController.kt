dae98a2d96b4ab82181a0ace69da77e8da3f2843
takahiro.tominaga <tt.300607@gmail.com>
2023-03-18 21:31:14 +0900

    scan devices

---
+++ BluetoothChatapp/app/src/main/java/com/example/bluetoothchatapp/domain/chat/BluetoothController.kt
@@ -0,0 +1,14 @@
+package com.example.bluetoothchatapp.domain.chat
+
+import kotlinx.coroutines.flow.StateFlow
+
+interface BluetoothController {
+
+    val scannedDevices: StateFlow<List<BluetoothDevice>>
+    val pairedDevices: StateFlow<List<BluetoothDevice>>
+
+    fun startDiscovery()
+    fun stopDiscovery()
+
+    fun release()
+}
