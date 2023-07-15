ad95974309fd11c9516fef00597bf005585068e4
takahiro.tominaga <tt.300607@gmail.com>
2023-07-15 17:56:54 +0900

    feat: MaterialTheme for Android and iOS

--- ContactsList/shared/src/commonMain/kotlin/App.kt
+++ ContactsList/shared/src/commonMain/kotlin/App.kt
@@ -1,10 +1,26 @@
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import core.presentation.ContactsTheme
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 @OptIn(ExperimentalResourceApi::class)
 @Composable
-fun App() {
-
+fun App(
+    darkTheme: Boolean,
+    dynamicColor: Boolean,
+) {
+    ContactsTheme(
+        darkTheme = darkTheme,
+        dynamicColor = dynamicColor,
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background,
+        ) {}
+    }
 }
 
 expect fun getPlatformName(): String
