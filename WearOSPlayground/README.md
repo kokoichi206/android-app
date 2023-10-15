b5411c54c44e021abd406398b62857444be1d698
takahiro.tominaga <tt.300607@gmail.com>
2023-10-15 11:52:43 +0900

    tried some method

---
+++ WearOSPlayground/README.md
@@ -0,0 +1,6 @@
+## Links
+
+- [pathways](https://developer.android.com/courses/pathways/wear?hl=ja)
+- [training/wearables](https://developer.android.com/training/wearables)
+- [wear-os-samples](https://github.com/android/wear-os-samples/tree/main)
+- [DroidKaigi2022](https://www.youtube.com/watch?v=o0HhsnVAGNA&ab_channel=DroidKaigi)
--- WearOSPlayground/app/src/main/java/jp/mydns/kokoichi206/wearosplayground/presentation/MainActivity.kt
+++ WearOSPlayground/app/src/main/java/jp/mydns/kokoichi206/wearosplayground/presentation/MainActivity.kt
@@ -10,60 +10,125 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.wear.compose.foundation.lazy.AutoCenteringParams
+import androidx.wear.compose.foundation.lazy.rememberScalingLazyListState
 import androidx.wear.compose.material.MaterialTheme
+import androidx.wear.compose.material.PositionIndicator
+import androidx.wear.compose.material.Scaffold
 import androidx.wear.compose.material.Text
-import jp.mydns.kokoichi206.wearosplayground.R
+import androidx.wear.compose.material.TimeText
+import androidx.wear.compose.material.Vignette
+import androidx.wear.compose.material.VignettePosition
+import com.google.android.horologist.annotations.ExperimentalHorologistApi
+import com.google.android.horologist.compose.layout.ScalingLazyColumn
+import com.google.android.horologist.compose.layout.ScalingLazyColumnDefaults
+import com.google.android.horologist.compose.layout.scrollAway
+import com.google.android.horologist.compose.rotaryinput.rotaryWithScroll
 import jp.mydns.kokoichi206.wearosplayground.presentation.theme.WearOSPlaygroundTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            WearApp("Android")
+            WearOSPlaygroundTheme {
+                WearApp()
+//                WearAppHoro()
+            }
         }
     }
 }
 
+@OptIn(ExperimentalHorologistApi::class)
 @Composable
-fun WearApp(greetingName: String) {
-    WearOSPlaygroundTheme {
-        /* If you have enough items in your list, use [ScalingLazyColumn] which is an optimized
-         * version of LazyColumn for wear devices with some added features. For more information,
-         * see d.android.com/wear/compose.
-         */
-        Column(
+fun WearApp() {
+    val focusRequester = remember { FocusRequester() }
+    val columnState = rememberScalingLazyListState(initialCenterItemIndex = 0)
+
+    Scaffold(
+        // スクロール位置を表示する。
+        positionIndicator = {
+            PositionIndicator(scalingLazyListState = columnState)
+        },
+        timeText = {
+            TimeText()
+        },
+        modifier = Modifier
+            .background(MaterialTheme.colors.background),
+        vignette = {
+            Vignette(
+                // 比較のため上にだけ周辺減光を入れる。
+                vignettePosition = VignettePosition.Top,
+            )
+        },
+    ) {
+        // 画面の上部と下部で要素が小さくなる。
+        androidx.wear.compose.foundation.lazy.ScalingLazyColumn(
             modifier = Modifier
-                .fillMaxSize()
-                .background(MaterialTheme.colors.background),
-            verticalArrangement = Arrangement.Center
+                .rotaryWithScroll(columnState, focusRequester)
+                .fillMaxWidth(),
+            state = columnState,
+            // デフォルトでは idx=1 から始まる。
+            autoCentering = AutoCenteringParams(itemIndex = 0)
         ) {
-            Greeting(greetingName = greetingName)
+            items(15) { i ->
+                Text(text = "content $i")
+            }
+        }
+
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
         }
     }
 }
 
+
+@OptIn(ExperimentalHorologistApi::class)
 @Composable
-fun Greeting(greetingName: String) {
-    Text(
-        modifier = Modifier.fillMaxWidth(),
-        textAlign = TextAlign.Center,
-        color = MaterialTheme.colors.primary,
-        text = stringResource(R.string.hello_world, greetingName)
-    )
+fun WearAppHoro() {
+    val columnState = ScalingLazyColumnDefaults.belowTimeText().create()
+
+    Scaffold(
+        timeText = {
+            TimeText(
+                modifier = Modifier
+                    .fillMaxSize()
+                    // https://github.com/android/wear-os-samples/blob/86978edd1e253d8d1022607f54db0ec6a113968a/ComposeAdvanced/app/src/main/java/com/example/android/wearable/composeadvanced/presentation/ui/watchlist/WatchListScreen.kt#L109
+                    .scrollAway(columnState)
+            )
+        },
+        modifier = Modifier
+            .background(MaterialTheme.colors.background),
+        vignette = {
+            Vignette(
+                // 比較のため上にだけ周辺減光を入れる。
+                vignettePosition = VignettePosition.Top,
+            )
+        },
+    ) {
+        ScalingLazyColumn(
+            modifier = Modifier
+                .fillMaxWidth(),
+            columnState = columnState,
+        ) {
+            items(15) { i ->
+                Text(text = "content $i")
+            }
+        }
+    }
 }
 
 @Preview(device = Devices.WEAR_OS_SMALL_ROUND, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
-    WearApp("Preview Android")
+    WearApp()
 }
