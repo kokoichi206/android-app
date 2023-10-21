6adc3f290f4d4d106afa0b720445789d2abd6fd6
takahiro.tominaga <tt.300607@gmail.com>
2023-10-21 10:40:39 +0900

    pinch zoom gestures

--- PlayGround/app/src/main/java/jp/mydns/kokoichi0206/playground/CoilCacheImage.kt
+++ PlayGround/app/src/main/java/jp/mydns/kokoichi0206/playground/CoilCacheImage.kt
@@ -1,5 +1,8 @@
 package jp.mydns.kokoichi0206.playground
 
+import androidx.compose.foundation.gestures.rememberTransformableState
+import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
@@ -7,10 +10,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.compose.SubcomposeAsyncImage
@@ -21,17 +28,59 @@ fun CoilCacheImage(
 ) {
     val imgUrl = "https://avatars.githubusercontent.com/u/52474650?v=4"
 
+    var scale by remember {
+        mutableStateOf(1f)
+    }
+    var rotation by remember {
+        mutableStateOf(1f)
+    }
+    var offset by remember {
+        mutableStateOf(Offset.Zero)
+    }
+
+
+
     Column(
         modifier = Modifier
             .fillMaxSize(),
     ) {
-        AsyncImage(
-            model = imgUrl,
-            contentDescription = null,
+        BoxWithConstraints(
             modifier = Modifier
                 .fillMaxWidth()
-//            .aspectRatio(1280f/847f)
-        )
+                .aspectRatio(1280f / 847f)
+        ) {
+            val state = rememberTransformableState { zoomChange, panChange, rotationChange ->
+                scale = (scale * zoomChange).coerceIn(1f, 5f)
+
+                rotation += rotationChange
+
+                val extraWidth = (scale - 1) * constraints.maxWidth
+                val extraHeight = (scale - 1) * constraints.maxHeight
+
+                val maxX = extraWidth / 2
+                val maxY = extraHeight / 2
+
+                offset = Offset(
+                    x = (offset.x + scale * panChange.x).coerceIn(-maxX, maxX),
+                    y = (offset.y + scale * panChange.y).coerceIn(-maxY, maxY)
+                )
+            }
+
+            AsyncImage(
+                model = imgUrl,
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .graphicsLayer {
+                        scaleX = scale
+                        scaleY = scale
+                        rotationZ = rotation
+                        translationX = offset.x
+                        translationY = offset.y
+                    }
+                    .transformable(state),
+            )
+        }
 
 //        SubcomposeAsyncImage(
 //            model = imgUrl,
